ff33cc4392955cfe8e881847ffe093671a598e15
Per Christian Moen <per.christian.moen@nav.no>
2024-02-12 17:45:40 +0100

    Legger til en superenkel afp offentlig mock
    Co-authored-by: Hans Magnus Hallaråker <hans.magnus.hallaraker@nav.no>

---
+++ src/main/kotlin/no/nav/pensjon/afpoffentlig/mock/afp_offentlig_mock/AfpOffentligStatusController.kt
@@ -0,0 +1,55 @@
+package no.nav.pensjon.afpoffentlig.mock.afp_offentlig_mock
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.Month
+import java.time.temporal.TemporalAdjuster
+import java.time.temporal.TemporalAdjusters
+
+@RestController
+@RequestMapping("/api/apf-offentlig-status")
+class AfpOffentligStatusController {
+    @PostMapping("/hent")
+    fun hentAfpOffentligStatus(@RequestBody request: HentStatusRequest): InnvilgetDto {
+        return InnvilgetDto(
+            belop = 50_000,
+            tpNummer = 3010,
+            startdato = LocalDate.now().with(TemporalAdjusters.firstDayOfNextMonth()),
+            sluttdato = null,
+            sistRegulert = LocalDate.now().minusYears(1).with(Month.MAY).withDayOfMonth(1),
+        )
+    }
+
+    data class HentStatusRequest(
+        val fnr: String,
+        val onsketVirkningtidspunkt: LocalDate,
+        val hjemmel: String,
+        val tema: String,
+        val type: String,
+    )
+
+    data class HentStatusResponse(
+        val innvilget: InnvilgetDto?,
+        val soknad: SoknadDto?,
+        val manglendeApi: ManglendeApiDto?,
+    )
+
+    data class InnvilgetDto(
+        val belop: Int,
+        val tpNummer: Int,
+        val startdato: LocalDate,
+        val sluttdato: LocalDate?,
+        val sistRegulert: LocalDate,
+    )
+
+    data class SoknadDto(
+        val onsketVirkningsdato: LocalDate,
+    )
+
+    data class ManglendeApiDto(
+        val onsketVirkningsdato: LocalDate
+    )
+}
