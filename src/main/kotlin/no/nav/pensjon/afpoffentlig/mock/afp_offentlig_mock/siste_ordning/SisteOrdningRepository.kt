b6f5e6e714cccb61c703db4dec974b12d644b3e7
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2024-06-28 09:20:20 +0200

    Drop table siste ordning. Skal ikke benyttes
    Co-authored-by: Per Christian Moen <per.christian.moen@nav.no>

--- src/main/kotlin/no/nav/pensjon/afpoffentlig/mock/afp_offentlig_mock/siste_ordning/SisteOrdningRepository.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.pensjon.afpoffentlig.mock.afp_offentlig_mock.siste_ordning
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-
-interface SisteOrdningRepository : JpaRepository<SisteOrdning, String>
-
