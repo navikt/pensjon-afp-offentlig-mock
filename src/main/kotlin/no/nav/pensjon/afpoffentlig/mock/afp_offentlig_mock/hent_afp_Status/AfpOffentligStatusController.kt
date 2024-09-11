e5c1de12650b773fa153d672d5dd86e57f73cbaa
Per Christian Moen <per.christian.moen@nav.no>
2024-09-11 17:16:58 +0200

    Skiller ut mock-oppsett i en egen kontroller med egen sti
    Co-authored-by: Hans Magnus Hallaråker <hans.magnus.hallaraker@nav.no>

--- src/main/kotlin/no/nav/pensjon/afpoffentlig/mock/afp_offentlig_mock/hent_afp_Status/AfpOffentligStatusController.kt
+++ src/main/kotlin/no/nav/pensjon/afpoffentlig/mock/afp_offentlig_mock/hent_afp_Status/AfpOffentligStatusController.kt
@@ -1,11 +1,7 @@
 package no.nav.pensjon.afpoffentlig.mock.afp_offentlig_mock.hent_afp_Status
 
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -31,25 +27,6 @@ class AfpOffentligStatusController(
                 sistBenyttetG = null,
                 belopsListe = null,
             )
-    }
-
-    @GetMapping("/{fnr}")
-    fun hentMockForPerson(@PathVariable("fnr") fnr: String): ResponseEntity<AfpOffentligStatusMockOppsett?> {
-        return ResponseEntity.ofNullable(hentAfpStatusRepository.findById(fnr).getOrNull()?.mockOppsett)
-    }
-
-    @PutMapping("/{fnr}")
-    fun lagreMockForPerson(
-        @PathVariable("fnr") fnr: String,
-        @RequestBody status: AfpOffentligStatusMockOppsett
-    ) {
-        hentAfpStatusRepository.deleteById(fnr)
-        hentAfpStatusRepository.save(PersonMockOppsett(fnr, status))
-    }
-
-    @DeleteMapping("/{fnr}")
-    fun slettMockForPerson(@PathVariable("fnr") fnr: String) {
-        hentAfpStatusRepository.deleteById(fnr)
     }
 }
 
