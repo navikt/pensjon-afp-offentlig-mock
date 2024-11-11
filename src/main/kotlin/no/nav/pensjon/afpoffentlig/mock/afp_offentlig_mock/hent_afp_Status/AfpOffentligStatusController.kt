13cdf1c4033d0ba36a6480ccb807ba9535aa8c6b
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2024-11-11 16:36:54 +0100

    Lagt til uttaksdato
    Co-authored-by: Per Christian Moen <per.christian.moen@nav.no>

--- src/main/kotlin/no/nav/pensjon/afpoffentlig/mock/afp_offentlig_mock/hent_afp_Status/AfpOffentligStatusController.kt
+++ src/main/kotlin/no/nav/pensjon/afpoffentlig/mock/afp_offentlig_mock/hent_afp_Status/AfpOffentligStatusController.kt
@@ -3,6 +3,7 @@ package no.nav.pensjon.afpoffentlig.mock.afp_offentlig_mock.hent_afp_Status
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import kotlin.jvm.optionals.getOrNull
 
@@ -11,10 +12,11 @@ import kotlin.jvm.optionals.getOrNull
 class AfpOffentligStatusController(
     val hentAfpStatusRepository: HentAfpStatusRepository,
 ) {
-    @GetMapping("/{tpNr}/hentAfpStatus/{fnr}")
+    @GetMapping("/{tpNr}/hentAfpStatus/{fnr}/{uttaksdato}")
     fun hentAfpOffentligStatus(
         @PathVariable("tpNr") tpNr: String,
         @PathVariable("fnr") fnr: String,
+        @RequestParam("uttaksdato") uttaksdato: String,
     ): AfpStatusResponse? {
         return hentAfpStatusRepository.findById(fnr).getOrNull()?.mockOppsett?.mocksvar?.firstOrNull { it.tpId == tpNr }
             ?: AfpStatusResponse(
